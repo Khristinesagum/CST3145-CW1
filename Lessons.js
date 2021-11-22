91037e6f7cd5e97627b1ac9a1fc02db2964d0e23
Khristine Sagum <tinsagum11@gmail.com>
2021-11-22 16:32:04 +0400

    Adding the files

---
+++ Lessons.js
@@ -0,0 +1,83 @@
+let lessons = [
+    {
+        "id": 1001,
+        "subject": "Math",
+        "Location": "London",
+        "Price": 100,
+        "Spaces": 5,
+        "image": "SubjectsImages/Math.PNG"
+    },
+    {
+        "id": 1002,
+        "subject": "English",
+        "Location": "York",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "SubjectsImages/English.png"
+    },
+    {
+        "id": 1003,
+        "subject": "Music",
+        "Location": "London",
+        "Price": 100,
+        "Spaces": 5,
+        "image": "SubjectsImages/Music.jpg"
+    },
+    {
+        "id": 1004,
+        "subject": "Physical Education",
+        "Location": "York",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "SubjectsImages/PE.jpg"
+    },
+    {
+        "id": 1005,
+        "subject": "Arts",
+        "Location": "London",
+        "Price": 100,
+        "Spaces": 5,
+        "image": "SubjectsImages/Arts.jpg"
+    },
+    {
+        "id": 1006,
+        "subject": "History",
+        "Location": "York",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "SubjectsImages/History.png"
+    },
+    {
+        "id": 1007,
+        "subject": "Geography",
+        "Location": "London",
+        "Price": 100,
+        "Spaces": 5,
+        "image": "SubjectsImages/Geography.PNG"
+    },
+    {
+        "id": 1008,
+        "subject": "Computer",
+        "Location": "York",
+        "Price": 80,
+        "Spaces": 5,
+        "image": "SubjectsImages/Computer.png"
+    },
+    {
+        "id": 1009,
+        "subject": "Literacy",
+        "Location": "London",
+        "Price": 74,
+        "Spaces": 5,
+        "image": "SubjectsImages/Literacy.PNG"
+    },
+    {
+        "id": 1010,
+        "subject": "Science",
+        "Location": "York",
+        "Price": 75,
+        "Spaces": 5,
+        "image": "SubjectsImages/Science.png"
+    }
+
+]
